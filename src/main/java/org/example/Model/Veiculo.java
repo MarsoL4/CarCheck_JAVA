03ca72edef14e2bf0ea4912819ffa338b679b21a
Marsol4 <enzogmarsola@gmail.com>
2024-11-04 18:53:52 -0300

    Arruma erros no Model do Veiculo

--- src/main/java/org/example/Model/Veiculo.java
+++ src/main/java/org/example/Model/Veiculo.java
@@ -8,6 +8,11 @@ public class Veiculo {
     String ano_fabricacao;
     String numero_chassi;
 
+    // Construtor padrão
+    public Veiculo() {
+    }
+
+    // Construtor completo
     public Veiculo(int veiculo_id, int cliente_id, String marca, String modelo, String ano_fabricacao, String numero_chassi) {
         this.veiculo_id = veiculo_id;
         this.cliente_id = cliente_id;
@@ -17,6 +22,7 @@ public class Veiculo {
         this.numero_chassi = numero_chassi;
     }
 
+    // Getters
     public int getVeiculo_id() {
         return veiculo_id;
     }
@@ -39,5 +45,30 @@ public class Veiculo {
 
     public String getNumero_chassi() {
         return numero_chassi;
+    }
+
+    // Setters
+    public void setVeiculo_id(int veiculo_id) {
+        this.veiculo_id = veiculo_id;
+    }
+
+    public void setCliente_id(int cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setAno_fabricacao(String ano_fabricacao) {
+        this.ano_fabricacao = ano_fabricacao;
+    }
+
+    public void setNumero_chassi(String numero_chassi) {
+        this.numero_chassi = numero_chassi;
     }
 }
